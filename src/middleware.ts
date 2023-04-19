4c85d1606cba6df7f831f68a8216500564ec8f98
slevrin <979379492@qq.com>
2023-04-19 22:28:14 +0800

    chore: test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import createI18nMiddleware from 'next-intl/middleware'
 
 export default createI18nMiddleware({
-  locales: ['en', 'zh'],
-  defaultLocale: 'en',
+  locales: ['en-US', 'zh-CN'],
+  defaultLocale: 'en-US',
 })
 
 export const config = {
